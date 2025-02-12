8cddd114dd23527d36321100726fdab56866abe7
BiancaLZC <160909595+BiancaLZC@users.noreply.github.com>
2025-02-12 15:33:53 +0200

    test hw 05

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,5 +2,6 @@ const { validateRequestBody } = require('./validateRequestBody');
 const isEmptyBody = require('./IsEmptyBody')
 const authenticate = require("./authenticate");
 const { isValidId } = require("./isValidId");
+const upload = require("./upload");
 
-module.exports = { validateRequestBody, isValidId, isEmptyBody, authenticate };
+module.exports = { validateRequestBody, isValidId, isEmptyBody, authenticate, upload};
